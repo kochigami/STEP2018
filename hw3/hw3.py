4075fd90bc8ecc70f191d64251bc02209c127d40
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-06-12 16:09:28 +0900

    add methods to calculate equation with brackets

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -104,7 +104,6 @@ def tokenize(line):
             print 'Invalid character found: ' + line[index]
             exit(1)
         tokens.append(token)
-    print tokens
     '''
     (3.0+4*(2-1))/5 =>
     [{'type': 'LEFTBRACKET'}, {'type': 'NUMBER', 'number': 3.0}, {'type': 'PLUS'}, {'type': 'NUMBER', 'number': 4}, {'type': 'TIMES'}, {'type': 'LEFTBRACKET'}, {'type': 'NUMBER', 'number': 2}, {'type': 'MINUS'}, {'type': 'NUMBER', 'number': 1}, {'type': 'RIGHTBRACKET'}, {'type': 'RIGHTBRACKET'}, {'type': 'DIVIDED'}, {'type': 'NUMBER', 'number': 5}]
@@ -133,9 +132,13 @@ def evaluate_times_and_divided(tokens):
                     token = {'type': 'NUMBER', 'number': tokens[index - 1]['number'] * tokens[index + 1]['number']}
                 elif tokens[index]['type'] == 'DIVIDED':
                     token = {'type': 'NUMBER', 'number': tokens[index - 1]['number'] / float(tokens[index + 1]['number'])}                
-                tokens[index] = token
-                del tokens[index - 1]
-                del tokens[index]
+                tokens[index] = token #1
+                del tokens[index - 1] #2
+                del tokens[index]     #3
+                # 0: [{'type': 'NUMBER', 'number': 2}, {'type': 'PLUS'}, {'type': 'NUMBER', 'number': 1}, {'type': 'DIVIDED'}, {'type': 'NUMBER', 'number': 2}, {'type': 'TIMES'}, {'type': 'NUMBER', 'number': 2}]
+                # 1: [{'type': 'NUMBER', 'number': 2}, {'type': 'PLUS'}, {'type': 'NUMBER', 'number': 1}, {'type': 'NUMBER', 'number': 0.5}, {'type': 'NUMBER', 'number': 2}, {'type': 'TIMES'}, {'type': 'NUMBER', 'number': 2}]
+                # 2: [{'type': 'NUMBER', 'number': 2}, {'type': 'PLUS'}, {'type': 'NUMBER', 'number': 0.5}, {'type': 'NUMBER', 'number': 2}, {'type': 'TIMES'}, {'type': 'NUMBER', 'number': 2}]
+                # 3: [{'type': 'NUMBER', 'number': 2}, {'type': 'PLUS'}, {'type': 'NUMBER', 'number': 0.5}, {'type': 'TIMES'}, {'type': 'NUMBER', 'number': 2}]
                 index -= 1
         index += 1
     return tokens
@@ -170,6 +173,45 @@ def evaluate_plus_and_minus(tokens):
         index += 1
     return answer
 
+def search_bracket(tokens):
+    brackets=[]
+    for index in range(len(tokens)):
+        if tokens[index]['type'] == 'LEFTBRACKET':
+            brackets.append([index])
+
+    tokens_index = 0
+    if len(brackets) > 0:
+        brackets_index = len(brackets) -1
+        while tokens_index < len(tokens) and brackets_index > -1:
+            if tokens[tokens_index]['type'] == 'RIGHTBRACKET':
+                brackets[brackets_index].append(tokens_index)
+                brackets_index -= 1
+            tokens_index += 1
+
+    return brackets
+
+def calculate_inside_bracket(tokens, brackets):
+    tokens_cp = []
+    delete_components = 0
+    for i in range(len(brackets)-1, -1, -1):
+        # tokens_cp = tokens[5] ~ tokens[9]
+        tokens_cp = []
+        for j in range(brackets[i][0] + 1, brackets[i][1] - delete_components):
+            tokens_cp.append(tokens[j])
+
+        # tokens_cp = evaluate_times_and_divided(tokens_cp)
+        # tokens_cp = evaluate_plus_and_minus(tokens_cp)
+        tokens_cp = evaluate_times_and_divided(tokens_cp)
+        tokens_cp = evaluate_plus_and_minus(tokens_cp)
+        tokens[brackets[i][0]] = {'type': 'NUMBER', 'number': tokens_cp}
+
+        # delete components of tokens[5]-[9] and put tokens_cp
+        for j in range(brackets[i][0] + 1, brackets[i][1] + 1 - delete_components):
+            del tokens[brackets[i][0] + 1]
+        delete_components += (brackets[i][1] + 2 - delete_components -1) - (brackets[i][0] + 1)
+
+    return tokens
+
 '''
 test
 
@@ -187,6 +229,8 @@ input [{'type': 'NUMBER', 'number': 3.2}, {'type': 'PLUS'} {'type': 'NUMBER', 'n
 
 def test(line, expectedAnswer):
     tokens = tokenize(line)
+    brackets = search_bracket(tokens)
+    tokens = calculate_inside_bracket(tokens, brackets)
     tokens = evaluate_times_and_divided(tokens)
     actualAnswer = evaluate_plus_and_minus(tokens)
     if abs(actualAnswer - expectedAnswer) < 1e-8:
@@ -225,6 +269,12 @@ def runTest():
 
     # plus & times & divided
     test("2+1/2*2", 3)
+
+    # brackets
+    test("(((1)))", 1)
+    test("(3.0+4*(2-1))/5", 1.4)
+    test("3.0+(2.1-10/100)", 5.0)
+
     print "==== Test finished! ====\n"
 
 runTest()
@@ -234,13 +284,10 @@ while True:
     line = raw_input()
     tokens = tokenize(line)
 
-    # TODO delete brackets
-    # fetch pair of brackets (left, right) (ex. [[5, 9], [0, 10]])
+    # fetch pair of brackets (left, right) (ex. [[0, 10], [5, 9]])
+    brackets = search_bracket(tokens)
     # loop & and calculate inside bracket
-    # tokens_cp = tokens[5] ~ tokens[9]
-    # tokens_cp = evaluate_times_and_divided(tokens_cp)
-    # tokens_cp = evaluate_plus_and_minus(tokens_cp)
-    # delete components of tokens[5]-[9] and put tokens_cp
+    tokens = calculate_inside_bracket(tokens, brackets)
 
     tokens = evaluate_times_and_divided(tokens)
     answer = evaluate_plus_and_minus(tokens)
