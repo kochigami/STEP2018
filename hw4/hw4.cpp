2fff0a88502218be722816e30f8d10a50d9461bc
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-06-20 17:37:45 +0900

    [cpp] first commit

---
+++ hw4/hw4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+// LINKS_FILE_PATH="./wikipedia/links.txt"
+// PAGES_FILE_PATH="./wikipedia/pages.txt"
+// DATA_SIZE = 1483277
+
+// make dictionary word-number <= pages
+
+// make adjacent list <= links
+
+// input word
+
+// convert word to number
+
+// dequeue
+
+// enqueue
+
+// search
+
+int main()
+{
+  std::cout << "hello" <<std::endl;
+}
